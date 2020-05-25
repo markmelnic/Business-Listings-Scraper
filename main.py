ba5d1b1abc8bc95dd2934650352702397b149ab7
Mark Melnic <markhappy200@gmail.com>
2020-05-25 21:36:12 +0300

    Write specific first line in csv file if non existent

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 
+import csv
 from businessesforsale_com import businessesforsale_com
 
 if __name__=='__main__':
@@ -7,6 +8,22 @@ if __name__=='__main__':
         links = linksFile.read().splitlines()
         linksFile.close()
 
+    # check if file is empty and write first line
+    try:
+        with open("results.csv", "r", newline='') as resultsFile:
+            data = resultsFile.read()
+            resultsFile.close()
+    except:
+        with open("results.csv", "a", newline='') as resultsFile:
+            data = ''
+            resultsFile.close()
+        
+    if data == '':
+        with open("results.csv", "a", newline='') as resultsFile:
+            csvWriter = csv.writer(resultsFile)
+            csvWriter.writerow(["Source", "State", "Region", "Title", "Description", "Real estate", "Reason for selling", "Employees", "Year", "Price", "Revenue", "EBITDA", "Cash flow", "Inventory", "FFE", "Result", "Contact"])
+            resultsFile.close()
+                    
     # find link corresponding function
     for link in links:
         if "businessesforsale" in link:
